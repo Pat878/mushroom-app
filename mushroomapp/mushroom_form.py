884c0ead6f92ebb2239f3309a34c20be7a7e1ac8
Pat Mellon <pat@shiftlabny.com>
2019-10-30 08:59:15 -0400

    form updates

--- mushroomapp/mushroom_form.py
+++ mushroomapp/mushroom_form.py
@@ -8,21 +8,29 @@ from mushroomapp.request_prediction import RequestPrediction
 
 class MushroomForm(forms.Form):
     capshape = forms.ChoiceField(label='Cap Shape', choices=[('b', 'bell'),
-                                                             ('c', 'conical'), 
-                                                             ('x',  'convex'), ('f', 'flat'),
+                                                             ('c', 'conical'),
+                                                             ('x',  'convex'), ('f',
+                                                                                'flat'),
                                                              ('k', 'knobbed'), ('s', 'sunken')])
     capsurface = forms.ChoiceField(label='Cap Surface', choices=[('f', 'fibrous'), ('g', 'grooves'),
                                                                  ('y', 'scaly'), ('s', 'smooth')])
     capcolor = forms.ChoiceField(label='Cap Color', choices=[('n', 'brown'), ('b', 'buff'),
-                                                             ('c', 'cinnamon'), ('g','gray'), ('r', 'green'), 
-                                                             ('p','pink'), ('u', 'purple'),
-                                                             ('e','red'), ('w','white'),('y', 'yellow')])                                                          
+                                                             ('c', 'cinnamon'), ('g',
+                                                                                 'gray'), ('r', 'green'),
+                                                             ('p', 'pink'), ('u',
+                                                                             'purple'),
+                                                             ('e', 'red'), ('w', 'white'), ('y', 'yellow')])
+    bruises = forms.ChoiceField(label='Bruises', choices=[
+                                ('t', 'yes'), ('f', 'no')])
+    odor = forms.ChoiceField(label='Odor', choices=[('a', 'almond'), ('l', 'anise'), ('c', 'creosote'), (
+        'y', 'fishy'), ('f', 'foul'), ('m', 'musty'), ('n', 'none'), ('p', 'pungent'), ('s', 'spicy')])
+    gillattachment = forms.ChoiceField(label='Gill Attachment', choices=[(
+        'a', 'attached'), ('d', 'descending'), ('f', 'free'), ('n', 'notched')])
+    gillspacing = forms.ChoiceField(label='Gill Spacing', choices=[(
+        'c', 'close'), ('w', 'crowded'), ('d', 'distant')])
+    gillsize = forms.ChoiceField(label='Gill Size', choices=[(
+        'b', 'broad'), ('n', 'narrow')])
 
-#bruises?: bruises=t,no=f
-#odor: almond=a,anise=l,creosote=c,fishy=y,foul=f, musty=m,none=n,pungent=p,spicy=s
-# gill-attachment: attached=a,descending=d,free=f,notched=n
-# gill-spacing: close=c,crowded=w,distant=d
-# gill-size: broad=b,narrow=n
 # gill-color: black=k,brown=n,buff=b,chocolate=h,gray=g, green=r,orange=o,pink=p,purple=u,red=e, white=w,yellow=y
 # stalk-shape: enlarging=e,tapering=t
 # stalk-root: bulbous=b,club=c,cup=u,equal=e, rhizomorphs=z,rooted=r,missing=?
@@ -30,13 +38,17 @@ class MushroomForm(forms.Form):
 # stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
 # stalk-color-above-ring: brown=n,buff=b,cinnamon=c,gray=g,orange=o, pink=p,red=e,white=w,yellow=y
 # stalk-color-below-ring: brown=n,buff=b,cinnamon=c,gray=g,orange=o, pink=p,red=e,white=w,yellow=y
-# veil-type: partial=p,universal=u
-# veil-color: brown=n,orange=o,white=w,yellow=y
+    veiltype = forms.ChoiceField(label='Veil Type', choices=[(
+        'p', 'partial'), ('u', 'universal')])
+    veilcolor = forms.ChoiceField(label='Veil Color', choices=[(
+        'n', 'brown'), ('o', 'orange'), ('w', 'white'), ('y', 'yellow')])
 # ring-number: none=n,one=o,two=t
 # ring-type: cobwebby=c,evanescent=e,flaring=f,large=l, none=n,pendant=p,sheathing=s,zone=z
 # spore-print-color: black=k,brown=n,buff=b,chocolate=h,green=r, orange=o,purple=u,white=w,yellow=y
 # population: abundant=a,clustered=c,numerous=n, scattered=s,several=v,solitary=y
-# habitat: grasses=g,leaves=l,meadows=m,paths=p, urban=u,waste=w,woods=d
+    habitat = forms.ChoiceField(label='Habitat', choices=[(
+        'g', 'grasses'), ('l', 'leaves'), ('m', 'meadows'), ('p', 'paths'), ('u', 'urban'), ('w', 'waste'), ('d', 'woods')])
+
 
 def form(request):
     submitted = False
