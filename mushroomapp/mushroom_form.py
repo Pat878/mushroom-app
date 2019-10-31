42e3dd78e0553f664ba848454e6f5d1d07f05773
Pat Mellon <pat@shiftlabny.com>
2019-10-30 20:40:10 -0400

    update

--- mushroomapp/mushroom_form.py
+++ mushroomapp/mushroom_form.py
@@ -31,21 +31,35 @@ class MushroomForm(forms.Form):
     gillsize = forms.ChoiceField(label='Gill Size', choices=[(
         'b', 'broad'), ('n', 'narrow')])
 
-# gill-color: black=k,brown=n,buff=b,chocolate=h,gray=g, green=r,orange=o,pink=p,purple=u,red=e, white=w,yellow=y
-# stalk-shape: enlarging=e,tapering=t
-# stalk-root: bulbous=b,club=c,cup=u,equal=e, rhizomorphs=z,rooted=r,missing=?
-# stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
-# stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
-# stalk-color-above-ring: brown=n,buff=b,cinnamon=c,gray=g,orange=o, pink=p,red=e,white=w,yellow=y
-# stalk-color-below-ring: brown=n,buff=b,cinnamon=c,gray=g,orange=o, pink=p,red=e,white=w,yellow=y
+    gillcolor = forms.ChoiceField(label='Gill Color', choices=[('k', 'black'), ('n', 'brown'), ('b', 'buff'),
+                                                               ('h', 'chocolate'), ('g', 'gray'), ('r', 'green'), (
+                                                                   'o', 'orange'), ('p', 'pink'), ('u', 'purple'), ('e', 'red'),
+                                                               ('w', 'white'), ('y', 'yellow')])
+    stalkshape = forms.ChoiceField(label='Stalk Shape', choices=[
+                                   ('e', 'enlarging'), ('t', 'tapering')])
+    stalkroot = forms.ChoiceField(label='Stalk Root', choices=[('b', 'bulbous'), ('c', 'club'), ('u', 'cup'),
+                                                               ('e', 'equal'), ('z', 'rhizomorphs'), ('r', 'rooted'), ('?', 'missing')])
+    stalksurfaceabovering = forms.ChoiceField(label='Stalk Surface Above Ring', choices=[('f', 'fibrous'),
+                                                                                   ('y', 'scaly'), ('k', 'silky'), ('s', 'smooth')])
+    stalksurfacebelowring = forms.ChoiceField(label='Stalk Surface Below Ring', choices=[('f', 'fibrous'),
+                                                                                   ('y', 'scaly'), ('k', 'silky'), ('s', 'smooth')])
+
+    stalkcolorabovering = forms.ChoiceField(label='Stalk Color Above Ring', choices=[('n', 'brown'), ('b', 'buff'),
+                                                                                     ('c', 'cinnamon'), ('g', 'gray'), ('o', 'orange'), ('p', 'pink'), ('e', 'red'), ('w', 'white'), ('y', 'yellow')])
+    stalkcolorbelowring = forms.ChoiceField(label='Stalk Color Below Ring', choices=[('n', 'brown'), ('b', 'buff'),
+                                                                                     ('c', 'cinnamon'), ('g', 'gray'), ('o', 'orange'), ('p', 'pink'), ('e', 'red'), ('w', 'white'), ('y', 'yellow')])
     veiltype = forms.ChoiceField(label='Veil Type', choices=[(
         'p', 'partial'), ('u', 'universal')])
     veilcolor = forms.ChoiceField(label='Veil Color', choices=[(
         'n', 'brown'), ('o', 'orange'), ('w', 'white'), ('y', 'yellow')])
-# ring-number: none=n,one=o,two=t
-# ring-type: cobwebby=c,evanescent=e,flaring=f,large=l, none=n,pendant=p,sheathing=s,zone=z
-# spore-print-color: black=k,brown=n,buff=b,chocolate=h,green=r, orange=o,purple=u,white=w,yellow=y
-# population: abundant=a,clustered=c,numerous=n, scattered=s,several=v,solitary=y
+    ringnumber = forms.ChoiceField(label='Ring Number', choices=[
+                                   ('n', 'none'), ('o', 'one'), ('t', 'two')])
+    ringtype = forms.ChoiceField(label='Ring Type', choices=[('c', 'cobwebby'), ('e', 'evanescent'), ('f', 'flaring'),
+                                                             ('l', 'large'), ('n', 'none'), ('p', 'pendant'), ('s', 'sheathing'), ('z', 'zone')])
+    sporeprintcolor = forms.ChoiceField(label='Spore Print Color', choices=[('k', 'black'), ('n', 'brown'),
+                                                                            ('b', 'buff'), ('h', 'chocolate'), ('r', 'green'), ('o', 'orange'), ('u', 'purple'), ('w', 'white'), ('y', 'yellow')])
+    population = forms.ChoiceField(label='Population', choices=[('a', 'abundant'), (
+        'c', 'clustered'), ('n', 'numerous'), ('s', 'scattered'), ('v', 'several'), ('y', 'solitary')])
     habitat = forms.ChoiceField(label='Habitat', choices=[(
         'g', 'grasses'), ('l', 'leaves'), ('m', 'meadows'), ('p', 'paths'), ('u', 'urban'), ('w', 'waste'), ('d', 'woods')])
 
